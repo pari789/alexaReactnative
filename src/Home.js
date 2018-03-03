1dc303edd1d80ca3a019bcf4e1d3ae1cb8dbb1b9
pari789 <pari2031998@gmail.com>
2018-03-03 22:29:54 +0530

    first commit

--- src/Home.js
+++ src/Home.js
@@ -1,48 +1,85 @@
-import React,{Component} from 'react';
-import Moment from 'react-moment';
-import {StyleSheet} from 'react-native';
-import {Text,View,Container,Header,Button,Body,Content,Card,CardItem,Left,Thumbnail,Right} from 'native-base';
-window.navigator.userAgent = 'react-native';
-export default class Home extends Component{
+import React, { Component } from 'react';
+
+import { AppRegistry, StyleSheet, ActivityIndicator, ListView, Text, View, Alert } from 'react-native';
+import {Container,Header,Button,Body,Content,Card,CardItem,Left,Thumbnail,Right} from 'native-base';
+export default class Home extends Component {
+
   constructor(props) {
-      super(props);
-
-      this.getData();
-      this.state = { logsList: [] };
-      this.LogData = [];
-  }
-
-  getData = () => {
-      setTimeout(() => {
-          fetch("https://bot.defect94.hasura-app.io/logs")
-              .then(response => {
-                  if (response.ok) {
-                      return response.json();
-                  }
-                  throw new Error('Network response was not ok.');
-              })
-              .then((response) => {
-                  if (!isEmpty(this.LogData)) {
-                      if (response.timestamp !== this.LogData[0].timestamp && !isEmpty(response.timestamp))
-                          this.LogData.unshift(response);
-                  } else {
-                      this.LogData.unshift(response);
-                  }
-
-                  this.setState({ logsList: this.LogData });
-                  this.getData();
-              }).catch((err) => {
-                  this.getData();
-              });
-      }, 1000);
+    super(props);
+    this.state = {
+      isLoading: true
+    }
   }
 
 
 
-  render(){
+  componentDidMount() {
+    var url = "https://data.incipiently69.hasura-app.io/v1/query";
+    console.log('Making data query (get logs )');
+    var requestOptions = {
+        "method": "POST",
+        "headers": {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer f214defa75df22ab6c5dd2c471e2e25cd9a2e6dc30b4fb79"
+        }
+    };
 
-    let{ state } = this;
-    return(
+    var body = {
+        "type": "select",
+        "args": {
+            "table": "logs",
+            "columns": [
+                "*"
+            ]
+        }
+    };
+
+    requestOptions["body"] = JSON.stringify(body);
+    console.log('Data Response ---------------------');
+
+    fetch(url, requestOptions)
+
+    .then(function(response) {
+      return response.json();
+    })
+
+      .then((responseJson) => {
+        let ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+        this.setState({
+          isLoading: false,
+          dataSource: ds.cloneWithRows(responseJson),
+        }, function() {
+          // In this block you can do something with new state.
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  ListViewItemSeparator = () => {
+    return (
+      <View
+        style={{
+          height: .5,
+          width: "100%",
+          backgroundColor: "#000",
+        }}
+      />
+    );
+  }
+
+
+  render() {
+    if (this.state.isLoading) {
+      return (
+        <View style={{flex: 1, paddingTop: 20}}>
+          <ActivityIndicator />
+        </View>
+      );
+    }
+
+    return (
       <Container>
       <Header style={styles.head}>
       <Left>
@@ -53,73 +90,48 @@ export default class Home extends Component{
         <Text style={{color: 'white' , fontSize:20, paddingTop:10 }}>Alexa:TMDB</Text>
         <Right/>
       </Header>
-      <Content>
-  {state.logsList.map((singleQuery, i) => (
-        <Card key={i}>
-        <CardItem style={styles.car}>
-        <Left>
-          <Text style={styles.text}>Time</Text>
-        </Left>
-        <Body />
-        <Right style={styles.time_style} >
-            <Text style={styles.time_text} > {singleQuery.timestamp} </Text>
-        </Right>
-          </CardItem>
-          <CardItem style={styles.car}>
-          <Left>
-            <Text style={styles.text}>Query</Text>
-          </Left>
-          <Right>
-              <Text style={styles.time_text} > {singleQuery.query_text} </Text>
-          </Right>
+      <View style={styles.MainContainer}>
 
-            </CardItem>
-            <CardItem style={styles.car}>
-            <Left>
-              <Text style={styles.text}>Answer</Text>
-            </Left>
-            <Right>
-            <Text>
-                {singleQuery.answer}
-            </Text>
-            </Right>
-              </CardItem>
-              <CardItem style={styles.car}>
-              <Left>
-                <Text style={styles.text}>Response Time</Text>
-              </Left>
-              <Right>
-              <Text > {singleQuery.response_time} seconds  </Text>
-              </Right>
-              </CardItem>
-              </Card>
+        <ListView
 
-            ))}
-                          </Content>
-              </Container>
+          dataSource={this.state.dataSource}
+
+          renderSeparator= {this.ListViewItemSeparator}
+          renderRow={(rowData) => <Text style={styles.rowViewContainer}
+          >Answer:  {rowData.answer}{"\n"}Response Time:  {rowData.response_time}{"\n"}Query:  {rowData.query_text}</Text>}
 
 
 
+        />
 
+      </View>
+      </Container>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  car:{
-    backgroundColor:'#D5F5E3',
 
-  },
-  head:{
-    backgroundColor:'#2ECC71'
+MainContainer :{
 
-  },
-  text:{
-   fontWeight:'bold',
-    fontSize:15, paddingTop:10
-  },
-    time_style: { flexDirection: 'row', justifyContent: 'flex-end' },
-    time_text: { fontSize: 10 }
-}
+// Setting up View inside content in Vertically center.
+justifyContent: 'center',
+flex:1,
+margin: 10
 
-)
+},
+
+   rowViewContainer: {
+        fontSize: 20,
+        paddingRight: 10,
+        paddingTop: 10,
+        paddingBottom: 10,
+        backgroundColor:'#D5F5E3',
+        fontWeight:'bold',
+      },
+      head:{
+        backgroundColor:'#2ECC71'
+
+      },
+
+});
